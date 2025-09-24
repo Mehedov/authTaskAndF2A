7bb554d84c1878ff9b1915c0ad9cd2367d644d76
Mehedov <mehedov.dev@yandex.ru>
2025-09-24 12:38:55 +0300

    feat: Initial display of the form of authorization

---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Form2FA from '../Form/Form2FA'
+import FormLogin from '../Form/FormLogin'
+
+export default function Auth() {
+	const [step] = useState<'login' | '2fa'>('login')
+
+	if (step === '2fa') {
+		return <Form2FA />
+	}
+	return step === 'login' ? <FormLogin /> : <Form2FA />
+}
